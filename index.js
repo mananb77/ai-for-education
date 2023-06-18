5c72aee605e10794a26983b2dd1e18f6542767f5
Manan Bhargava <manan.bhargava@berkeley.edu>
2023-06-18 16:06:33 -0700

    made some final updates

--- index.js
+++ index.js
@@ -15,33 +15,79 @@ const configuration = new Configuration({
 
 const openai = new OpenAIApi(configuration);
 
+
+// Get DOM elements
+const userInputBox = document.getElementById('user-input-box');
+const sendButton = document.getElementById('send-button');
+const chatLog = document.getElementById('chat-log');
+
+// Function to append a message to the chat log
+function appendMessage(sender, message) {
+  const messageElement = document.createElement('div');
+  messageElement.classList.add('message');
+  messageElement.innerHTML = `<strong>${sender}: </strong>${message}`;
+  chatLog.appendChild(messageElement);
+}
+
+// Function to handle user input and generate chatbot response
+async function handleUserInput() {
+  const userInput = userInputBox.value;
+  appendMessage('You', userInput);
+
+  setTimeout(async () => {
+    // Simulating a chatbot response (replace with your actual logic)
+    const chatbotResponse = await generateChatbotResponse(userInput);
+    appendMessage('Chatbot', chatbotResponse);
+
+    // Clear the user input box
+    userInputBox.value = '';
+  }, 1000); // Change the delay duration as per your requirement
+}
+
+// Event listener for the send button click
+sendButton.addEventListener('click', handleUserInput);
+
+// Event listener for the Enter key press in the user input box
+userInputBox.addEventListener('keydown', (event) => {
+  if (event.key === 'Enter') {
+    event.preventDefault(); // Prevent form submission
+    handleUserInput();
+  }
+});
+
+// Function to generate a chatbot response (replace with your actual logic)
+async function generateChatbotResponse(userInput) {
+  // Replace this with your actual logic to generate the chatbot response
+  return "Sure, I've created a calendar generated from your link that you attached.";
+}
+
+
 app.post("/", async (req, res) => {
+  const options = {
+    method: "POST",
+    headers: {
+      "Authorization": `Bearer ${API_KEY}`,
+      "Content_Type": "application/json"
+    },
+    body: JSON.stringify({
+      model: "gpt-3.5-turbo",
+      messages: [
+        {role: "system", content: "You are courseLogisticsGPT, a helpful assistant for developing todo lists and calendar outlines for developing and running courses."},
+        {role: "user", content: message}
+      ],
+      ... message,
+      temperature: 0,
+      max_tokens: 100,
+    })
+  }
 
-//   const options = {
-//     method: "POST",
-//     headers: {
-//       "Authorization": `Bearer ${API_KEY}`,
-//       "Content_Type": "application/json"
-//     },
-//     body: JSON.stringify({
-//       model: "gpt-3.5-turbo",
-//       messages: [
-//         {role: "system", content: "You are courseLogisticsGPT, a helpful assistant for developing todo lists and calendar outlines for developing and running courses."},
-//         {role: "user", content: message}
-//       ],
-//       ... message,
-//       temperature: 0,
-//       max_tokens: 100,
-//     })
-//   }
-
-//   try {
-//     const response = await fetch('https://api.openai.com/v1/chat/completions', options)
-//     const data = await response.json()
-//     res.send(data)
-//   } catch (error) {
-//     console.error(error)
-//   }
+  try {
+    const response = await fetch('https://api.openai.com/v1/chat/completions', options)
+    const data = await response.json()
+    res.send(data)
+  } catch (error) {
+    console.error(error)
+  }
 
   const { message } = req.body;
 
@@ -64,95 +110,6 @@ app.post("/", async (req, res) => {
 
 });
 
-// // Get DOM elements
-// const userInputBox = document.getElementById('user-input-box');
-// const sendButton = document.getElementById('send-button');
-// const chatLog = document.getElementById('chat-log');
-
-// // Function to append a message to the chat log
-// function appendMessage(sender, message) {
-//   const messageElement = document.createElement('div');
-//   messageElement.classList.add('message');
-//   messageElement.innerHTML = `<strong>${sender}: </strong>${message}`;
-//   chatLog.appendChild(messageElement);
-// }
-
-// // Function to handle user input and generate chatbot response
-// async function handleUserInput() {
-//   const userInput = userInputBox.value;
-//   appendMessage('You', userInput);
-
-//   // Add user input to conversation history
-//   conversation.push({
-//     role: 'system',
-//     content: userInput
-//   });
-
-//   // Generate chatbot response using OpenAI API
-//   const response = await getChatbotResponse(conversation);
-
-//   // Append chatbot response to the chat log
-//   const chatbotReply = response.choices[0].message.content;
-//   appendMessage('Chatbot', chatbotReply);
-
-//   // Clear user input box
-//   userInputBox.value = '';
-
-//   // Scroll to the bottom of the chat log
-//   chatLog.scrollTop = chatLog.scrollHeight;
-// }
-
-// // Function to send conversation history to OpenAI API and get chatbot response
-// async function getChatbotResponse(conversation) {
-//   try {
-//     const response = await openai.ChatCompletion.create({
-//       model: 'gpt-4', // Replace with the desired model ID
-//       messages: [
-//         {role: "system", content: "You are courseLogisticsGPT, a helpful assistant for developing todo lists and calendar outlines for developing and running courses."},
-//         {role: user, content: conversation}
-//       ],
-//       apiKey: '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',
-//     });
-
-//     return response;
-//   } catch (error) {
-//     console.error('Error:', error);
-//     // Handle error
-//   }
-// }
-
-// // Event listener for the send button
-// sendButton.addEventListener('click', handleUserInput);
-
-// // Event listener for the enter key press
-// userInputBox.addEventListener('keypress', function(event) {
-//   if (event.key === 'Enter') {
-//     handleUserInput();
-//   }
-// });
-
-// // Function to scroll to the bottom of the chat log
-// function scrollToBottom() {
-//   chatLog.scrollTop = chatLog.scrollHeight;
-// }
-
-// // Scroll to the bottom of the chat log on initial load and when new messages are appended
-// document.addEventListener('DOMContentLoaded', scrollToBottom);
-// chatLog.addEventListener('DOMNodeInserted', scrollToBottom);
-
-
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 });
-
-
-
-// lets do a single query
-// then pass the query response into the calendar / gmail integration
-
-
-
-
-
-
-
